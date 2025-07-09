378cb7d2b338c895ee05b9ee89d41d96756899b6
xdendix <dendiprayogo24@gmail.com>
2025-07-09 12:32:17 +0700

    add new files in inhertance folder

--- src/inheritance/main.py
+++ src/inheritance/main.py
@@ -1,28 +1,25 @@
-class Karyawan:
-    def __init__(self, nama, umur, posisi):
-        self.nama = nama
-        self.umur = umur
-        self.posisi = posisi
+class Mobil:
+    def __init__(self, warna, merek, kecepatan):
+        self.warna = warna
+        self.merek = merek
+        self.kecepatan = kecepatan
 
-    def lihat_data(self):
-        print(f"""
-        Nama: {self.nama}
-        Umur: {self.umur}
-        Posisi: {self.posisi}
-        """)
+    def tambah_kecepatan(self):
+        self.kecepatan += 10
 
 
-class KaryawanTetap(Karyawan):
-    asuransi_pensiun = ""
+class MobilSport(Mobil):
+    def turbo(self):
+        self.kecepatan += 50
 
 
-class KaryawanKontrak(Karyawan):
-    durasi_kontrak = 0
+# Kelas Mobil Dasar
+mobil_1 = Mobil("Hitam", "Mustang", 3000)
+print(mobil_1.kecepatan)
 
+# Kelas Mobil Sport
+mobil_sport_1 = MobilSport("Hitam", "Ferari", 5000)
+print(mobil_sport_1.kecepatan)
 
-class KaryawanOutsourcing:
-    vendor = ""
-
-
-karyawan_kontrak = KaryawanKontrak("Dendi", 30, "Programmer")
-karyawan_kontrak.lihat_data()
+mobil_sport_1.turbo()
+print(mobil_sport_1.kecepatan)
